b2c851c0e2e8c99b7d47b25be8e980bb2b930233
HuynhHoangAn <huynhhoangan.cantho@gmail.com>
2019-02-17 20:52:43 +0700

    xu-li-muc-bai-tap-da-lam

--- public/javascript/show-exe-of-topic.js
+++ public/javascript/show-exe-of-topic.js
@@ -1,7 +1,28 @@
-var myTopic = document.querySelector('.topic'); /* chon div co class là topic */
-var myTable = document.querySelector('.showOneTopic');  /* chọn bảng chứa thông tin sau khi nhấn vào div.topic */
-if (myTable && myTopic) {
-    myTopic.addEventListener('click', function() {
-        myTable.classList.add('showExeTopic');
-    });
-}
+function openTopic(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    document.getElementById("btnOnTop").style.display = "block";
+  } else {
+    document.getElementById("btnOnTop").style.display = "none";
+  }
+}  
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}  
